e65236e7c3e58226ab391bbeee7b67f113deca65
Gally <70110451+CianGallagher@users.noreply.github.com>
2022-09-29 14:51:08 +0100

    imported and implemented material UI for nicer buttons and font

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseLine from "@mui/material/Button";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <CssBaseLine />
     <App />
   </React.StrictMode>
 );
